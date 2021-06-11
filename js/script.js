0a7075caa96a8d0440c5fb683f810276248fe5b5
alex <bugrov24an@yandex.ru>
2021-06-11 14:41:40 +0300

    So, that is my improovement web-APP

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 "use strict";
 
-let numberOfFilms = +prompt('How many films have you already watched?');
+let numberOfFilms = +prompt('How many films have you already watched?', '');
+
+if (numberOfFilms < 10) {
+    alert('You have watched quite little amount of films');
+    } else if (numberOfFilms >= 10 && numberOfFilms < 20) {
+        alert('You are a several person who loves watching films');
+    } else {
+        alert('Looks like you are mad about watching films! Congrats!');
+    }
+
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,11 +19,39 @@ let personalMovieDB = {
     privat: false
 };
 
-let lastFilm = prompt('What was your last film?'),
-    assess = prompt('How do you assess it?'),
-    lastFilm2 = prompt('What was your last film?'),
-    assess2 = prompt('How do you assess it?');
+for (let i = 0; i < 2; i++) {
 
-personalMovieDB.movies[lastFilm] = assess;
-personalMovieDB.movies[lastFilm2] = assess2;
+    let a = prompt('What was your last film?', '');
+    
+    if (a != null && a != '' && a.length < 50) {
+        let b = prompt('How do you assess it on a scale from 1 to 5?', '');
+        
+        if (b != null && b != '' && b <= 5) {
+            personalMovieDB.movies[a] = b;
+            
+        } else {
+            alert('Looks like you insered empty data or too high mark :(');
+            i--;
+        }
+        
+    } else {
+        alert('You cant insert empty data');
+        i--;
+    }
+}
+
+// for (let i = 0; i < 2; i++) {
+
+//     let a = prompt('What was your last film?', ''),
+//         b = prompt('How do you assess it?', '');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         alert('Please insert correct data');
+//         i = 0;
+//     }
+// }
+
 console.log(personalMovieDB);
+
