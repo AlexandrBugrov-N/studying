18a1cfd5a4e8597b34a0382cc17bbfeaa9ba2aab
alex <bugrov24an@yandex.ru>
2021-06-15 14:10:51 +0300

    new commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 "use strict";
 
-let numberOfFilms = +prompt('How many films have you already watched?', '');
 
-if (numberOfFilms < 10) {
-    alert('You have watched quite little amount of films');
-    } else if (numberOfFilms >= 10 && numberOfFilms < 20) {
-        alert('You are a several person who loves watching films');
-    } else {
-        alert('Looks like you are mad about watching films! Congrats!');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many films have you already watched?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films have you already watched?', '');
     }
+}
+
+start();
+
 
 
 let personalMovieDB = {
@@ -16,30 +20,71 @@ let personalMovieDB = {
     movies: {},
     actors:{},
     genres: [],
-    privat: false
+    privat: true,
 };
 
-for (let i = 0; i < 2; i++) {
 
-    let a = prompt('What was your last film?', '');
-    
-    if (a != null && a != '' && a.length < 50) {
-        let b = prompt('How do you assess it on a scale from 1 to 5?', '');
-        
-        if (b != null && b != '' && b <= 5) {
-            personalMovieDB.movies[a] = b;
-            
+
+function detectPersonalLevel() {
+    if (numberOfFilms < 10) {
+        alert('You have watched quite little amount of films');
+        } else if (numberOfFilms >= 10 && numberOfFilms < 20) {
+            alert('You are a several person who loves watching films');
         } else {
-            alert('Looks like you insered empty data or too high mark :(');
-            i--;
+            alert('Looks like you are mad about watching films! Congrats!');
         }
-        
+}
+detectPersonalLevel();
+
+
+
+// Задание номер два
+function showMyDb() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
     } else {
-        alert('You cant insert empty data');
-        i--;
+        console.log('Sorry, this is personal moviedata base :(');
     }
 }
+showMyDb();
 
+
+
+// Задание номер три 
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`What is you favorite genre number ${i + 1}?`);
+    }
+}
+writeYourGenres();
+
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+
+        let a = prompt('What was your last film?', '');
+        
+        if (a != null && a != '' && a.length < 50) {
+            let b = prompt('How do you assess it on a scale from 1 to 5?', '');
+            
+            if (b != null && b != '' && b <= 5) {
+                personalMovieDB.movies[a] = b;
+                
+            } else {
+                alert('Looks like you inserted empty data or too high mark :(');
+                i--;
+            }
+            
+        } else {
+            alert('You cant insert empty data');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+// rememberMyFilms();
 // for (let i = 0; i < 2; i++) {
 
 //     let a = prompt('What was your last film?', ''),
@@ -53,5 +98,4 @@ for (let i = 0; i < 2; i++) {
 //     }
 // }
 
-console.log(personalMovieDB);
 
