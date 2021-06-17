bbcc2d0a31514fda95fce448015dd8a77e01364c
alex <bugrov24an@yandex.ru>
2021-06-17 16:57:19 +0300

    improving to object oriented approach

--- js/script.js
+++ js/script.js
@@ -1,89 +1,77 @@
 "use strict";
 
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('How many films have you already watched?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How many films have you already watched?', '');
-    }
-}
-
-start();
-
-
 
 let personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors:{},
     genres: [],
     privat: true,
-};
-
-
-
-function detectPersonalLevel() {
-    if (numberOfFilms < 10) {
+    countAmountOfFilms: function() {
+        personalMovieDB.count = +prompt('How many films have you already watched?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('How many films have you already watched?', '');
+        }
+    },
+    detectPersonalLevel: function() {
+    if (personalMovieDB.count < 10) {
         alert('You have watched quite little amount of films');
-        } else if (numberOfFilms >= 10 && numberOfFilms < 20) {
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 20) {
             alert('You are a several person who loves watching films');
         } else {
             alert('Looks like you are mad about watching films! Congrats!');
         }
-}
-detectPersonalLevel();
-
-
-
-// Задание номер два
-function showMyDb() {
-    if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
-    } else {
-        console.log('Sorry, this is personal moviedata base :(');
-    }
-}
-showMyDb();
-
-
-
-// Задание номер три 
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`What is you favorite genre number ${i + 1}?`);
-    }
-}
-writeYourGenres();
-
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-
-        let a = prompt('What was your last film?', '');
-        
-        if (a != null && a != '' && a.length < 50) {
-            let b = prompt('How do you assess it on a scale from 1 to 5?', '');
-            
-            if (b != null && b != '' && b <= 5) {
-                personalMovieDB.movies[a] = b;
-                
-            } else {
-                alert('Looks like you inserted empty data or too high mark :(');
+    },
+    showMyDb: function() {
+        if (personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        } else {
+            console.log('Sorry, this is personal movie data base :(');
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 0; i < 3; i++) {
+            personalMovieDB.genres[i] = prompt(`What is you favorite genre number ${i + 1}?`);
+            if (personalMovieDB.genres[i] == null || personalMovieDB.genres[i] == '') {
                 i--;
             }
+        }
+        personalMovieDB.genres.forEach((order, i) => {
+            console.log(`Your favorite genre number ${i} is ${order}`);
+        });
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+    
+            let a = prompt('What was your last film?', '');
             
+            if (a != null && a != '' && a.length < 50) {
+                let b = prompt('How do you assess it on a scale from 1 to 5?', '');
+                
+                if (b != null && b != '' && b <= 5) {
+                    personalMovieDB.movies[a] = b;
+                    
+                } else {
+                    alert('Looks like you inserted empty data or too high mark :(');
+                    i--;
+                }
+                
+            } else {
+                alert('You cant insert empty data');
+                i--;
+            }
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
         } else {
-            alert('You cant insert empty data');
-            i--;
+            personalMovieDB.privat = true;
         }
     }
-}
-rememberMyFilms();
-
+};
 // rememberMyFilms();
 // for (let i = 0; i < 2; i++) {
 
@@ -97,5 +85,3 @@ rememberMyFilms();
 //         i = 0;
 //     }
 // }
-
-
